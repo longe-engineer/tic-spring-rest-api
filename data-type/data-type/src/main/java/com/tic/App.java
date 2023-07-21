2115970331e4380b4016059ee93c9d641f6fb0d5
geneic-niue <s.suzuki@vrst.co.jp>
2023-07-22 00:54:54 +0900

    データ量増加

--- data-type/data-type/src/main/java/com/tic/App.java
+++ data-type/data-type/src/main/java/com/tic/App.java
@@ -1,15 +1,30 @@
 package com.tic;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import com.tic.service.BookCSVService;
 import com.tic.service.BookJSONService;
 
 @SuppressWarnings("unused")
 public class App {
-	private static final BookJSONService jsonService = new BookJSONService();
-	private static final BookCSVService csvService = new BookCSVService();
+    private static final BookJSONService jsonService = new BookJSONService();
+    private static final BookCSVService csvService = new BookCSVService();
 
     public static void main(String[] args) throws Exception {
 
-        csvService.changeBookStatus(1,2,3,4,5,6,7,8,9,10);
+        csvService.changeBookStatus(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // // 大量データの貸出状況を一括ランダム変更
+        // List<Integer> intlist = IntStream.range(0, 10000)
+        //         .boxed()
+        //         .map(i -> (int) (Math.random() * (double) 350))
+        //         .toList();
+        // long[] ints = new long[intlist.size()];
+        // intlist.forEach(i -> {
+        //     ints[intlist.indexOf(i)] = i;
+        //     System.out.println(i);
+        // });
+        // csvService.changeBookStatus(ints);
     }
 }
